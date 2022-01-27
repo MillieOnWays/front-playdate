67188be847a29a0a50f9ef10deffd4c7bf92c0ab
deepali-02 <deepali93.redekar@gmail.com>
2022-01-27 17:42:44 +0100

    added some styles

--- src/components/PlaydateForm/index.js
+++ src/components/PlaydateForm/index.js
@@ -1,12 +1,33 @@
 import { Container, Form, Image, Col, Row, Card } from "react-bootstrap";
 import { useState, useEffect } from "react";
 
+import topImg from "../../images/playdate1.jpg";
 
 export default function PlaydateForm() {
   const [imageUrl, setImageUrl] = useState("");
   return (
     <>
-      <Card className="mt-5 bg-info" style={{ width: "60rem" }}>
+      {/* <div>
+        <Image src={topImg} alt="playdate image" />
+      </div> */}
+      <Card
+        className="mt-5"
+        style={{
+          width: "70rem",
+          backgroundColor: "#FFB6C1",
+          //   backgroundImage: "url(" + bgImg + ")",
+          //   backgroundSize: "",
+          alignSelf: "center",
+          marginLeft: "17%",
+
+          borderWidth: "5px",
+        }}
+      >
+        <Card.Img
+          variant="top"
+          src={topImg}
+          style={{ width: "100%", height: "14vw" }}
+        />
         <Card.Header
           className="text-center"
           style={{ fontWeight: "bold", fontSize: "25px" }}
@@ -17,15 +38,15 @@ export default function PlaydateForm() {
           <Form>
             <Row className="mb-3">
               <Col>
-                <Form.Group controlId="formGridName">
+                <Form.Group className="mb-3" controlId="formGridName">
                   <Form.Label>Playdate Name</Form.Label>
                   <Form.Control
                     type="text"
                     placeholder="What you wanna call to your playdate"
                   />
                 </Form.Group>
-                <Form.Group controlId="formGridDate">
-                  <Form.Label>Choose Dtae</Form.Label>
+                <Form.Group className="mb-3" controlId="formGridDate">
+                  <Form.Label>Choose Date</Form.Label>
                   <Form.Control type="date" required />
                 </Form.Group>
                 <Row className="mb-3">
@@ -48,7 +69,7 @@ export default function PlaydateForm() {
                 </Form.Group>
               </Col>
               <Col>
-                <Row>
+                <Row className="mb-3">
                   <Col>
                     <Form.Group controlId="formGridImage">
                       <Form.Label>
@@ -62,7 +83,7 @@ export default function PlaydateForm() {
                     </Form.Group>
                   </Col>
                   <Col>
-                    <Form.Group className="mt-3">
+                    <Form.Group className="mt-4">
                       <Form.Label> </Form.Label>
                       <Image src={imageUrl} alt="preview" thumbnail />
                     </Form.Group>
