9350aa4f762bbedd1c2d2e9c4b2efff3977d23ca
cynster <marius.korff@live.nl>
2022-01-27 15:50:56 +0100

    Setup AllDates page/css; Playdate store; Route/link to page

---
+++ src/store/playdate/actions.js
@@ -0,0 +1,30 @@
+import { apiUrl, DEFAULT_PAGINATION_LIMIT } from "../../config/constants";
+import axios from "axios";
+import {
+  appLoading,
+  appDoneLoading,
+  showMessageWithTimeout,
+  setMessage,
+} from "../appState/actions";
+
+export const FETCH_PLAYDATES_SUCCESS = "FETCH_PLAYDATES_SUCCESS";
+
+export const fetchPlaydatesSuccess = (playdates) => ({
+  type: FETCH_PLAYDATES_SUCCESS,
+  payload: playdates,
+});
+
+export const fetchTrainers = () => {
+  return async (dispatch, getState) => {
+    try {
+      const playdatesCount = getState().playdates.allPlaydates.length;
+      const response = await axios.get(
+        `${apiUrl}/playdates?limit=${DEFAULT_PAGINATION_LIMIT}&offset=${playdatesCount}&order=ASC`
+      );
+
+      dispatch(fetchPlaydatesSuccess(response.data.playdates.rows));
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+};
