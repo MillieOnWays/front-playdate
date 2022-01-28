1e2f6bdc00f693caf6b472405805a5e155078d4a
deepali-02 <deepali93.redekar@gmail.com>
2022-01-28 12:33:35 +0100

    create new playdate and update the playdate store

--- src/store/playdate/reducer.js
+++ src/store/playdate/reducer.js
@@ -1,19 +1,26 @@
 import { FETCH_PLAYDATES_SUCCESS } from "./actions";
 
 const initialState = {
-    allPlaydates: [],
-  };
+  allPlaydates: [],
+};
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = initialState, action) => {
-    switch (action.type) {
-      case FETCH_PLAYDATES_SUCCESS:
-        return {
-            ...state,
-            allPlaydates: action.payload,
-          };
-    
-      default:
-        return state;
+  switch (action.type) {
+    case FETCH_PLAYDATES_SUCCESS:
+      return {
+        ...state,
+        allPlaydates: action.payload,
+      };
+
+    case "ADD_NEW_PLAYDATE": {
+      return {
+        ...state,
+        allPlaydates: [...state.allPlaydates, action.payload],
+      };
     }
-  };
+
+    default:
+      return state;
+  }
+};
