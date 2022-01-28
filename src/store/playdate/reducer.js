acfca62018c210cd3390ab51247afec9949c083c
MillieOnWays <l.udalova90@gmail.com>
2022-01-28 10:32:42 +0100

    redux actions,reducer and selectors updated for playdate details page

--- src/store/playdate/reducer.js
+++ src/store/playdate/reducer.js
@@ -1,19 +1,25 @@
 import { FETCH_PLAYDATES_SUCCESS } from "./actions";
+import { PLAYDATE_DETAILS_FETCHED } from "./actions";
 
 const initialState = {
-    allPlaydates: [],
-  };
+  allPlaydates: [],
+};
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = initialState, action) => {
-    switch (action.type) {
-      case FETCH_PLAYDATES_SUCCESS:
-        return {
-            ...state,
-            allPlaydates: action.payload,
-          };
-    
-      default:
-        return state;
-    }
-  };
+  switch (action.type) {
+    case FETCH_PLAYDATES_SUCCESS:
+      return {
+        ...state,
+        allPlaydates: action.payload,
+      };
+    case PLAYDATE_DETAILS_FETCHED:
+      return {
+        ...state,
+        details: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
