4dd37fe5f6665a2e28e386aef0885cad5d354e0b
deepali-02 <deepali93.redekar@gmail.com>
2022-01-28 23:04:59 +0100

    added style on Home,Login & Signup pages

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -6,7 +6,8 @@ import { signUp } from "../../store/user/actions";
 import { selectToken } from "../../store/user/selectors";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
-import { Col } from "react-bootstrap";
+import { Col, Card } from "react-bootstrap";
+import "./SignUp.css";
 
 export default function SignUp() {
   const [name, setName] = useState("");
@@ -33,50 +34,54 @@ export default function SignUp() {
   }
 
   return (
-    <Container>
-      <Form as={Col} md={{ span: 6, offset: 3 }} className='mt-5'>
-        <h1 className='mt-5 mb-5'>Signup</h1>
-        <Form.Group controlId='formBasicName'>
-          <Form.Label>Name</Form.Label>
-          <Form.Control
-            value={name}
-            onChange={(event) => setName(event.target.value)}
-            type='text'
-            placeholder='Enter name'
-            required
-          />
-        </Form.Group>
-        <Form.Group controlId='formBasicEmail'>
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            value={email}
-            onChange={(event) => setEmail(event.target.value)}
-            type='email'
-            placeholder='Enter email'
-            required
-          />
-          <Form.Text className='text-muted'>
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+    <Container className="mt-3">
+      <Card className="bgImg" style={{ width: "80rem", height: "40rem" }}>
+        <Form as={Col} md={{ span: 6, offset: 3 }} className="mt-3">
+          <h1 className="mb-3">Signup</h1>
+          <Form.Group controlId="formBasicName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+              type="text"
+              placeholder="Enter name"
+              required
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicEmail" className="mt-3">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
+              type="email"
+              placeholder="Enter email"
+              required
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
 
-        <Form.Group controlId='formBasicPassword'>
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            type='password'
-            placeholder='Password'
-            required
-          />
-        </Form.Group>
-        <Form.Group className='mt-5'>
-          <Button variant='primary' type='submit' onClick={submitForm}>
-            Sign up
-          </Button>
-        </Form.Group>
-        <Link to='/login'>Click here to log in</Link>
-      </Form>
+          <Form.Group controlId="formBasicPassword" className="mt-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              type="password"
+              placeholder="Password"
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mt-5">
+            <Button variant="primary" type="submit" onClick={submitForm}>
+              Sign up
+            </Button>
+          </Form.Group>
+          <Form.Group className="mt-3">
+            <Link to="/login">Click here to log in</Link>
+          </Form.Group>
+        </Form>
+      </Card>
     </Container>
   );
 }
