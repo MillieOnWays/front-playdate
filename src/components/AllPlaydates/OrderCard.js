3b62ad6ee82f2779913d2c48a2ec998c8c218e98
cynster <marius.korff@live.nl>
2022-01-31 19:23:51 +0100

    Setup OrderCard by Date

--- src/components/AllPlaydates/OrderCard.js
+++ src/components/AllPlaydates/OrderCard.js
@@ -1,63 +1,16 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import { Button, Col, Container, Row, Card, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import moment from "moment";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchPlaydates } from "../../store/playdate/actions";
+import React from "react";
+import { Button, Card } from "react-bootstrap";
 
-export default function OrderCard(playdate) {
-  const dispatch = useDispatch();
-
-  function firstLetterUpperCase(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-
-  const [order, setOrder] = useState("");
-  const [orderBy, setOrderBy] = useState("");
-
-  console.log("order outside function:", order, orderBy);
-
-  function changeOrder(e) {
-    console.log("triggered?", order, orderBy);
-    e.preventDefault();
-    //dispatch(fetchPlaydates(order, orderBy));
-  }
-  console.log("order outside AFTER function:", order, orderBy);
+export default function OrderCard(props) {
   return (
     <Card className="mt-5" bg="light">
       <Card.Header>
-        <b>Order</b>
+        <b>Sort by</b>
       </Card.Header>
       <Card.Body>
-        <Form onChange={changeOrder}>
-          <Form.Group controlId="formBasicSelect">
-            <Form.Control
-              as="select"
-              disabled
-              value={orderBy}
-              onChange={(e) => setOrderBy(e.target.value)}
-            >
-              <option value="latest">Latest</option>
-              <option value="title">Title</option>
-              <option value="user">Parent</option>
-              <option value="city">City</option>
-              <option value="date">Date</option>
-            </Form.Control>
-          </Form.Group>
-
-          <Form.Group controlId="formBasicSelect">
-            <Form.Control
-              as="select"
-              disabled
-              value={order}
-              onChange={(e) => setOrder(e.target.value)}
-            >
-              <option value="DESC">Descending ↓</option>
-              <option value="ASC">Ascending ↑</option>
-            </Form.Control>
-          </Form.Group>
-        </Form>
+        <Button style={{ width: "100%" }} onClick={props.sortPlaydatesBy}>
+          {props.toggle ? "Date ↑" : "Date ↓"}
+        </Button>
       </Card.Body>
     </Card>
   );
